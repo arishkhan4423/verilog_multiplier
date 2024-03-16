f25e4a249c7c90d205ebb23b4df3107483c5a3d3
arishkhan4423 <135980399+arishkhan4423@users.noreply.github.com>
2024-03-16 12:28:32 +0530

    Update vcode.v

--- vcode.v
+++ vcode.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:55:44 11/26/2021 
-// Design Name: 
-// Module Name:    project 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module multiplier(
 		out, a_in, b_in, clk, start, reset, finish, bcd
 	);
